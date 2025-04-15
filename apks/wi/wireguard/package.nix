dc819cd558d6d1d7b0f2deca16e2c872e8fb9eb4
osbm <osmanfbayram@gmail.com>
2025-04-15 19:37:22 +0300

    add proper wireguard source

--- apks/wi/wireguard/package.nix
+++ apks/wi/wireguard/package.nix
@@ -7,12 +7,10 @@
 stdenv.mkDerivation rec {
   name = "wireguard";
   version = "1.0.20231018";
-  src = pkgs.fetchgit {
-    url = "https://git.zx2c4.com/wireguard-android";
-    tag = "v${version}";
-    hash = "";
+  src = pkgs.fetchzip {
+    url = "https://git.zx2c4.com/wireguard-android/snapshot/wireguard-android-${version}.tar.xz";
+    sha256 = "sha256-09bXYljoXvwHn3QkzBjQHsIbvXrCzBceUSXVRRc8q8g=";
   };
-
 
   nativeBuildInputs = [
     pkgs.yarn

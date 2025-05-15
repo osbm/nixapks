eaa075fd9a95ed33dc6508d5acefd91c4ce0863e
osbm <osmanfbayram@gmail.com>
2025-05-15 16:09:54 +0300

    update flake

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     gradle2nix-flake.url = "github:tadfisher/gradle2nix/v2";
     gradle-dot-nix.url = "github:CrazyChaoz/gradle-dot-nix";
   };
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }@inputs:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -15,10 +15,13 @@
     };
     callPackage = pkgs.callPackage;
     stdenv = pkgs.stdenv;
+    
   in
   {
     packages.x86_64-linux.lichess = callPackage ./apks/by-name/li/lichess/package.nix { };
     packages.x86_64-linux.wireguard = callPackage ./apks/by-name/wi/wireguard/package.nix { };
-    packages.x86_64-linux.smouldering_durtles = callPackage ./apks/by-name/sm/smouldering_durtles/package.nix { };
+    packages.x86_64-linux.smouldering_durtles = callPackage ./apks/by-name/sm/smouldering_durtles/package.nix {
+      inherit (inputs) android-nixpkgs gradle-dot-nix;
+    };
   };
 }

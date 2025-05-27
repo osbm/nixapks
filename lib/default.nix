1f3819ed63ad0d8ef4b24f4cec9844bf63e34ea9
osbm <osmanfbayram@gmail.com>
2025-05-27 20:31:26 +0300

    formatting

--- lib/default.nix
+++ lib/default.nix
@@ -1,13 +1,16 @@
-{pkgs, ...}: {
-
-
- by-name-overlay = baseDirectory: let
-  namesForShard = shard: _:
-    pkgs.lib.mapAttrs (name: _: baseDirectory + "/${shard}/${name}/package.nix") (
-        builtins.readDir (baseDirectory + "/${shard}")
+{ pkgs, ... }:
+{
+  by-name-overlay =
+    baseDirectory:
+    let
+      namesForShard =
+        shard: _:
+        pkgs.lib.mapAttrs (name: _: baseDirectory + "/${shard}/${name}/package.nix") (
+          builtins.readDir (baseDirectory + "/${shard}")
+        );
+      packageFiles = pkgs.lib.mergeAttrsList (
+        pkgs.lib.attrsets.mapAttrsToList namesForShard (builtins.readDir baseDirectory)
       );
-    packageFiles = pkgs.lib.mergeAttrsList (pkgs.lib.attrsets.mapAttrsToList namesForShard (builtins.readDir baseDirectory));
-  in
-  pkgs.lib.mapAttrs (name: _: pkgs.callPackage _ { }) packageFiles;
-
+    in
+    pkgs.lib.mapAttrs (name: _: pkgs.callPackage _ { }) packageFiles;
 }

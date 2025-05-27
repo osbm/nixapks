ea746f320398ba67198ab5b27a466cc4c09d7cfe
osbm <osmanfbayram@gmail.com>
2025-05-27 20:21:38 +0300

    feat: add by-name-overlay function to default.nix

---
+++ lib/default.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}: {
+
+
+ by-name-overlay = baseDirectory: let
+  namesForShard = shard: _:
+    pkgs.lib.mapAttrs (name: _: baseDirectory + "/${shard}/${name}/package.nix") (
+        builtins.readDir (baseDirectory + "/${shard}")
+      );
+    packageFiles = pkgs.lib.mergeAttrsList (pkgs.lib.attrsets.mapAttrsToList namesForShard (builtins.readDir baseDirectory));
+  in
+  pkgs.lib.mapAttrs (name: _: pkgs.callPackage _ { }) packageFiles;
+
+}

4554009f45f01ee0c3a600ac5c9d6e5f49a90d33
osbm <osmanfbayram@gmail.com>
2025-05-27 16:58:12 +0300

    finally working

--- apks/by-name/sm/smouldering_durtles/package.nix
+++ apks/by-name/sm/smouldering_durtles/package.nix
@@ -1,16 +1,24 @@
-{lib, pkgs, android-nixpkgs, gradle-dot-nix,...}:
+{
+  lib,
+  pkgs,
+  android-nixpkgs,
+  gradle-dot-nix,
+  ...
+}:
 let
-  android-sdk = android-nixpkgs.sdk.${pkgs.stdenv.hostPlatform.system} (sdkPkgs: with sdkPkgs; [
-    # Useful packages for building and testing.
-    build-tools-34-0-0
-    cmdline-tools-latest
-    platform-tools
-    platforms-android-34
-  ]);
+  android-sdk = android-nixpkgs.sdk.${pkgs.stdenv.hostPlatform.system} (
+    sdkPkgs: with sdkPkgs; [
+      # Useful packages for building and testing.
+      build-tools-34-0-0
+      cmdline-tools-latest
+      platform-tools
+      platforms-android-34
+    ]
+  );
   gradle-init-script =
     (import gradle-dot-nix {
-        inherit pkgs;
-        gradle-verification-metadata-file = ./verification-metadata.xml;
+      inherit pkgs;
+      gradle-verification-metadata-file = ./verification-metadata.xml;
     }).gradle-init;
 in
 
@@ -24,20 +32,19 @@ pkgs.stdenv.mkDerivation rec {
     tag = "v${version}";
     hash = "sha256-xk8xjvUCpHojwdoaBhiXPfX2Tm1iXF8pbphk/FFt1P0=";
   };
-          JDK_HOME = "${pkgs.jdk21.home}";
-        ANDROID_HOME = "${android-sdk}/share/android-sdk";
+  JDK_HOME = "${pkgs.jdk21.home}";
+  ANDROID_HOME = "${android-sdk}/share/android-sdk";
 
-        nativeBuildInputs = [
-            android-sdk
-            pkgs.gradle
-            pkgs.jdk21
-        ];
-        buildPhase = ''
-          gradle build --info -I ${gradle-init-script} --offline --full-stacktrace -Dorg.gradle.project.android.aapt2FromMavenOverride=$ANDROID_HOME/build-tools/34.0.0/aapt2
-        '';
-        installPhase = ''
-          mkdir -p $out
-          cp -r ./app/build/outputs/apk/release/app-release-unsigned.apk $out
-        '';
+  nativeBuildInputs = [
+    android-sdk
+    pkgs.gradle_8
+    pkgs.jdk21
+  ];
+  buildPhase = ''
+    gradle build --info -I ${gradle-init-script} \
+      --offline --full-stacktrace -Dorg.gradle.project.android.aapt2FromMavenOverride=$ANDROID_HOME/build-tools/34.0.0/aapt2
+  '';
+  installPhase = ''
+    cp app/build/outputs/apk/release/app-release.apk $out
+  '';
 }
-
